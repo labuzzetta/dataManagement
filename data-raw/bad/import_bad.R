f066f5dbda2b35a3f3ec3588f6448d742cb4945d
clabuzze <clabuzze@iastate.edu>
2017-06-08 08:31:17 -0500

    Add devtools use_data

--- data-raw/bad/import_bad.R
+++ data-raw/bad/import_bad.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(tidyr)
 
 sightings_bad <- readr::read_csv(file = "bad/sightings_bad.csv")
-
 sightings_really_bad <- readr::read_csv(file = "bad/sightings_really_bad.csv")
-
 species_bad <- readr::read_csv(file = "bad/species_bad.csv")
+
+devtools::use_data(sightings_bad, sightings_really_bad, species_bad, overwrite=TRUE)
