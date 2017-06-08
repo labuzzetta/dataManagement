a2f5a91379a114150c7ad489bbdb62dd5cb6d69f
clabuzze <clabuzze@iastate.edu>
2017-06-08 08:33:10 -0500

    Import datasets using devtools

--- data-raw/good/import_good.R
+++ data-raw/good/import_good.R
@@ -3,15 +3,16 @@ library(readr)
 library(dplyr)
 library(tidyr)
 
+#read csv files
 locations <- readr::read_csv(file = "good/locations_good.csv")
-
 observers <- readr::read_csv(file = "good/observers_good.csv")
-
 sightings <- readr::read_csv(file = "good/sightings_good.csv")
-
 species <- readr::read_csv(file = "good/species_good.csv")
 
 #combine datasets
 full <- left_join(x = sightings, y = locations, "location_id") %>%
         left_join(y = observers, "observer_id") %>%
         left_join(y = species, "species_id")
+
+#Import datasets and save to package
+devtools::use_data(locations, observers, sightings, species, full, overwrite=TRUE)
