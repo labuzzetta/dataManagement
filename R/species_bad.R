31902cb75f3855cec594b9d53f4e286a1a839d33
clabuzze <clabuzze@iastate.edu>
2017-06-08 09:40:26 -0500

    Reflow comment

--- R/species_bad.R
+++ R/species_bad.R
@@ -3,16 +3,17 @@
 #' Contains species records with poor data management
 #' 
 #' @format A data.frame with the following columns:
-#' 
-#'   \code{ID}: Unique integer ID of bird species - Note is an integer ID
-#'              needed if the species abbreviation is unique?
-#'                    
-#'   \code{species Abbreviation}: Four letter abbreviation of species - Note space in column name
+#'   
+#'   \code{ID}: Unique integer ID of bird species - Note is an integer ID needed
+#'   if the species abbreviation is unique?
+#'   
+#'   \code{species Abbreviation}: Four letter abbreviation of species - Note
+#'   space in column name
 #'   
 #'   \code{genus}: Genus of bird
 #'   
 #'   \code{species}: Full name of species of bird
 #'   
 #'   \code{common name}: Full common name of species - Note space in column name
-#'  
+#'   
 "species_bad"
